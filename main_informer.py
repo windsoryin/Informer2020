e1ac4cee4705e11755a0cb8646442638f9faa3a9
windsoryin <1403538271@qq.com>
2023-04-25 15:58:38 +0800

    加入一行注释

--- main_informer.py
+++ main_informer.py
@@ -58,7 +58,9 @@ parser.add_argument('--gpu', type=int, default=0, help='gpu')
 parser.add_argument('--use_multi_gpu', action='store_true', help='use multiple gpus', default=False)
 parser.add_argument('--devices', type=str, default='0,1,2,3',help='device ids of multile gpus')
 
+#ssssss
 args = parser.parse_args()
+
 
 args.use_gpu = True if torch.cuda.is_available() and args.use_gpu else False
 
