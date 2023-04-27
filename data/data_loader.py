c45b03f7a788629226e9d1b1bb1359f179e91339
YUAN0620 <651667985@qq.com>
2023-04-27 09:23:39 +0800

    zhushi

--- data/data_loader.py
+++ data/data_loader.py
@@ -185,12 +185,16 @@ class Dataset_ETT_minute(Dataset):
         return self.scaler.inverse_transform(data)
 
 
+# 23/4/27 yc 对于我们而言，所使用到的就是Dataset_Custom这个类
 class Dataset_Custom(Dataset):
     def __init__(self, root_path, flag='train', size=None, 
                  features='S', data_path='ETTh1.csv', 
                  target='OT', scale=True, inverse=False, timeenc=0, freq='h', cols=None):
+        # 上面是通过设置的属性来进行传参
         # size [seq_len, label_len, pred_len]
         # info
+        # 如果没有定义以下3列的值，那么采用24*4*4，24*4，24*4，其中这个size就是在exp_informer.py的data_set中传入的参数；
+        # 如果定义了这3个参数的话，就直接传入；
         if size == None:
             self.seq_len = 24*4*4
             self.label_len = 24*4
@@ -216,7 +220,9 @@ class Dataset_Custom(Dataset):
         self.__read_data__()
 
     def __read_data__(self):
+        # 对数据进行标准化处理
         self.scaler = StandardScaler()
+        # 通过路径读取数据文件
         df_raw = pd.read_csv(os.path.join(self.root_path,
                                           self.data_path))
         '''
@@ -225,14 +231,18 @@ class Dataset_Custom(Dataset):
         # cols = list(df_raw.columns); 
         if self.cols:
             cols=self.cols.copy()
+            # 删掉target这列
             cols.remove(self.target)
         else:
             cols = list(df_raw.columns); cols.remove(self.target); cols.remove('date')
+        # 23/4/27理解：cols表示的就是other features！！！
         df_raw = df_raw[['date']+cols+[self.target]]
 
-        num_train = int(len(df_raw)*0.7)
+        # 划分训练集、测试集、验证集上的数据
+        num_train = int(len(df_raw)*0.6)
         num_test = int(len(df_raw)*0.2)
         num_vali = len(df_raw) - num_train - num_test
+        划分每个集的边界
         border1s = [0, num_train-self.seq_len, len(df_raw)-num_test-self.seq_len]
         border2s = [num_train, num_train+num_vali, len(df_raw)]
         border1 = border1s[self.set_type]
